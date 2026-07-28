a3d8a18fbb09add21a1b8537dfb65bca6eb1712e
Chimcha Titus <chimchatitus@gmail.com>
2026-07-28 17:31:32 +0200

    update

--- frontend/src/components/marketing/CampaignSlider/CampaignSlider.jsx
+++ frontend/src/components/marketing/CampaignSlider/CampaignSlider.jsx
@@ -19,6 +19,7 @@ import "swiper/css/navigation";
 import "swiper/css/pagination";
 
 
+
 import { getPublicCampaigns } from "../../../services/campaignService";
 
 import CampaignCard from "../CampaignCard/CampaignCard";
