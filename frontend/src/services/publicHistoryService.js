8cb270e0ef45f7ea30756aac862d2867136498d7
Chimcha Titus <chimchatitus@gmail.com>
2026-08-12 00:59:40 +0200

    update

--- frontend/src/services/publicHistoryService.js
+++ frontend/src/services/publicHistoryService.js
@@ -1,6 +1,6 @@
-import api from "./api"; // your existing axios instance
+import api from "./api";
 
 export const getHistory = async () => {
-  const { data } = await api.get("/api/gieva/history/all");
+  const { data } = await api.get("/api/gieva/history");
   return data;
 };
